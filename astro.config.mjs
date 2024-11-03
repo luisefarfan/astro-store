2f08fc49a1d477d287504dbf21c7d07e7336787c
LuisFarfan57 <luise.prado99@hotmail.com>
2024-11-02 20:07:47 -0600

    feat: add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
+// import netlify from '@astrojs/netlify';
 
 import db from '@astrojs/db';
 
@@ -10,9 +10,14 @@ import auth from 'auth-astro';
 
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
+  // adapter: netlify()
   integrations: [tailwind(), db(), auth(), react()],
-  adapter: netlify()
+
+  adapter: cloudflare()
 });
